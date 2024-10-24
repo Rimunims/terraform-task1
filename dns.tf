03935d4285358ed5a8fdad4a762e41aae21a811d
RimFei <armstrongrimun@gmail.com>
2024-10-24 11:49:52 -0500

    adjusted syntax

--- dns.tf
+++ dns.tf
@@ -1,12 +1,12 @@
 resource "google_dns_managed_zone" "resolver-zone"{
     name = "universal-resolver-zone"
-    dns_name = "universal-did-resolver"
+    dns_name = "universal-did-resolver."
 }
 resource "google_dns_record_set" "resolver-dns" {
-    name = "universal-did-resolver"
+    name = "universal-did-resolver."
     type = "A"
     ttl = 300
     managed_zone = google_dns_managed_zone.resolver-zone.name
 
-    rrdatas = [google_compute_global_address.resolver_global_ip.address]
+    rrdatas = [google_compute_global_address.resolver_global_ip]
 }
