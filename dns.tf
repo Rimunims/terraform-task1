d3cde0dc9f7c89f59b4c8c76012f65f06bb4a9a9
RimFei <armstrongrimun@gmail.com>
2024-10-25 12:17:47 -0500

    added deployment scripts

--- dns.tf
+++ dns.tf
@@ -1,12 +1,12 @@
 resource "google_dns_managed_zone" "resolver-zone"{
     name = "universal-resolver-zone"
-    dns_name = "universal-did-resolver."
+    dns_name = "dev.uniresolver.io."
 }
 resource "google_dns_record_set" "resolver-dns" {
-    name = "universal-did-resolver."
+    name = "dev.uniresolver.io."
     type = "A"
     ttl = 300
     managed_zone = google_dns_managed_zone.resolver-zone.name
 
-    rrdatas = [google_compute_global_address.resolver_global_ip]
+    rrdatas = [google_compute_global_address.resolver_global_ip.address]
 }
